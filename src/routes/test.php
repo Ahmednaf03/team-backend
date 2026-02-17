612c5b8d1a7c163faa41b36f8e7531975dc0807b
Ahmednaf03 <ahamednafees777@gmail.com>
2026-02-18 00:47:15 +0530

    gitignore

--- src/routes/test.php
+++ src/routes/test.php
@@ -1,9 +1,20 @@
 <?php
 
 $router->get('/api/test', function ($request, $response) {
+     $request->set('user', [
+        'user_id'   => 1,
+        // 'tenant_id' => 2,
+        'role'      => 'admin'
+    ]);
+
+    TenantMiddleware::handle($request,$response);
+    RoleMiddleware::handle($request,$response, ['admin']);
     $response->json([
         'status' => 'ok',
         'method' => $request->method(),
-        'uri' => $request->uri()
+        'uri' => $request->uri(),
+        'tenant_id' => $request->get('tenant_id'),
+        'message' => 'Middleware working',
+        'user' => $request->get('user')
     ]);
 });
